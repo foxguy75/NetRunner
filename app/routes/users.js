c0b4c5d909833b6b959890bddb93286d3789126f
Ben Camburn <foxguy75@gmail.com>
2016-03-19 19:41:02 +0000

    Updated users route module.

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,22 +1,40 @@
 var express = require("express")
   , router  = express.Router()
-  , User    = require("../models/user");
+  , db    = require("../models/user")
+  , User  = db.User
+  , Users = db.Users;
 
 // user list
 router.route("/users")
   .get(function (req, res) {
-    // TODO:
-    res.status(200).json();
+    Users.forge().fetch()
+      .then(function (users){
+        res.status(200).json(users.toJSON());
+      })
+      .catch(function (err) {
+       console.log(err);
+       res.status(500).json();
+      });
   })
   .post(function (req, res) {
     // TODO:
   });
 
 // user
-router.route("/users/:id")
+router.route("/user/:id")
   .get(function (req, res) {
-    // TODO:
-    res.status(200).json();
+    Users.forge().fetchOne({where: {id: req.params.id}})
+    .then(function (user) {
+      if (user) {
+        res.status(200).json(user.toJSON());
+      } else {
+        res.status(404).json();
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+      res.status(500).json();
+    });
   })
   .put(function (req, res) {
     // TODO:
